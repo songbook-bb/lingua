cd545e97df3660a86dea1131b210f660302e2e4f
naqn <tomek.nakonieczny@gmail.com>
2014-08-16 21:15:11 +0000

    better formatting
    git-svn-id: https://svn.code.sf.net/p/lingmem/code-0/trunk@37 a1c67283-34fb-429b-9957-cc89a7e5b9eb

--- src/wordtutor/container/WordVectorContainer.java
+++ src/wordtutor/container/WordVectorContainer.java
@@ -1,238 +1,231 @@
 package wordtutor.container;
-import java.util.*;
+
+import java.util.Vector;
+
 import wordtutor.util.TranslateDirection;
 
 /**
- * Implements the IWordContainer interface.
- * Has a nested class - WordElem which implements IWord interface
- * nested class can access to the fields of his owner and it uses
- * the maxScore, incScore and decScores fields to work with
- * score of the word
- * The storage uses java.util.vector to keep the words
- * @author moradan  
+ * Implements the IWordContainer interface. Has a nested class - WordElem which
+ * implements IWord interface nested class can access to the fields of his owner
+ * and it uses the maxScore, incScore and decScores fields to work with score of
+ * the word The storage uses java.util.vector to keep the words
+ * 
+ * @author moradan
  */
-public class WordVectorContainer implements IWordContainer{
+public class WordVectorContainer implements IWordContainer {
 
-	 /**
-	  * Nested class implementing IWord interface.
-	  * Uses the maxScore, incScore and decScores fields of owner class to work with
-      * score of the word
-	  */
-	class WordElem implements IWord
-	{
-		private String foreignWord;  
-		private String nativeWord;  
-		private int score;                  
-		private int idSound;		
-		private boolean isLearned;  
-		
+	/**
+	 * Nested class implementing IWord interface. Uses the maxScore, incScore
+	 * and decScores fields of owner class to work with score of the word
+	 */
+	class WordElem implements IWord {
+		private String foreignWord;
+		private String nativeWord;
+		private int score;
+		private int idSound;
+		private boolean isLearned;
+
 		/**
 		 * zeros the score of the word and marks it not learned
-		 *  
+		 * 
 		 */
-		private void initStatistics()
-		{
+		private void initStatistics() {
 			this.score = 0;
 			this.isLearned = false;
 		}
-		/** default constructor
-		 *  zeros the foreign and native spelling
+
+		/**
+		 * default constructor zeros the foreign and native spelling
 		 */
-		public WordElem() 
-		{
+		public WordElem() {
 			this.foreignWord = "";
 			this.nativeWord = "";
-		    initStatistics();
-		}
-		/**  constructor which creates the word 
-		 *  with foreign spelling  and native spelling 
-		 */
-		public WordElem(String foreignWord,String nativeWord, int idSound)
-		{
-		 this.foreignWord = foreignWord;
-		 this.nativeWord = nativeWord;
-		 this.idSound = idSound;
-		 initStatistics();
+			initStatistics();
 		}
 
-		/** marks the word learned
-		 *  
+		/**
+		 * constructor which creates the word with foreign spelling and native
+		 * spelling
 		 */
-		public void setLearned(boolean isLearned)
-		{
+		public WordElem(String foreignWord, String nativeWord, int idSound) {
+			this.foreignWord = foreignWord;
+			this.nativeWord = nativeWord;
+			this.idSound = idSound;
+			initStatistics();
+		}
+
+		/**
+		 * marks the word learned
+		 * 
+		 */
+		public void setLearned(boolean isLearned) {
 			this.isLearned = isLearned;
 		}
-		
-		 /**
-		  * tells if word is learned
-		  */
-		public boolean isLearned()
-		{
+
+		/**
+		 * tells if word is learned
+		 */
+		public boolean isLearned() {
 			return isLearned;
 		}
-		
+
 		/**
-		  * sets the foreign spelling of the word 
-		  */
-	    public	void setForeignWord(String foreignWord)
-		{
+		 * sets the foreign spelling of the word
+		 */
+		public void setForeignWord(String foreignWord) {
 			this.foreignWord = foreignWord;
 		}
-	    /**
-	     * gets the native spelling of the word 
-	     */
-		public String getNativeWord()
-		{
+
+		/**
+		 * gets the native spelling of the word
+		 */
+		public String getNativeWord() {
 			return this.nativeWord;
 		}
-	    /**
-	     * sets the native spelling of the word 
-	     */
-    	public void setNativeWord(String nativeWord)
-		{
+
+		/**
+		 * sets the native spelling of the word
+		 */
+		public void setNativeWord(String nativeWord) {
 			this.nativeWord = nativeWord;
 		}
-	    /**
-	     * gets the foreign spelling of the word 
-	     */
-		public String getForeignWord()
-		{
+
+		/**
+		 * gets the foreign spelling of the word
+		 */
+		public String getForeignWord() {
 			return this.foreignWord;
 		}
-	    /**
-	     * gets the current score of the word 
-	     */
-		public int getScore()
-		{
+
+		/**
+		 * gets the current score of the word
+		 */
+		public int getScore() {
 			return this.score;
 		}
-	    /**
-	     * sets the score of the word 
-	     */
-		public void setScore(int score)
-		{
+
+		/**
+		 * sets the score of the word
+		 */
+		public void setScore(int score) {
 			this.score = score;
 		}
-	    /**
-	     * gets the idSound of the word 
-	     */
-		public int getIdSound()
-		{
+
+		/**
+		 * gets the idSound of the word
+		 */
+		public int getIdSound() {
 			return this.idSound;
 		}
-	    /**
-	     * sets the idSound of the word 
-	     */
-		public void setIdSound(int idSound)
-		{
-			this.idSound = idSound;
-		}		
+
 		/**
-	     * compares the words 
-	     */
-//		private boolean wordEqual(String word1,String word2)
-//		{
-//			return word1.trim().equals(word2.trim());
-//		}
-		
-		 /**
-		  * checks the user's answer for corresponding the right answer
-		  * its work depends on the current direction of the translating.
-		  * If the direction is straight, this method will compare the answer of user
-		  * with all variants of translations of the word.
-		  * If the direction is reverse, then it will compare the answer just with
-		  * the foreign spelling
-		  * 
-		  * @param answer is the user's answer
-		  * @param direction is a direction of translation - straight or reverse
-    	  */
-    	public boolean checkAnswer(String answer,TranslateDirection direction)
-		{
+		 * sets the idSound of the word
+		 */
+		public void setIdSound(int idSound) {
+			this.idSound = idSound;
+		}
+
+		/**
+		 * compares the words
+		 */
+		// private boolean wordEqual(String word1,String word2)
+		// {
+		// return word1.trim().equals(word2.trim());
+		// }
+
+		/**
+		 * checks the user's answer for corresponding the right answer its work
+		 * depends on the current direction of the translating. If the direction
+		 * is straight, this method will compare the answer of user with all
+		 * variants of translations of the word. If the direction is reverse,
+		 * then it will compare the answer just with the foreign spelling
+		 * 
+		 * @param answer
+		 *            is the user's answer
+		 * @param direction
+		 *            is a direction of translation - straight or reverse
+		 */
+		public boolean checkAnswer(String answer, TranslateDirection direction) {
 			boolean isRight = true;
-//			if (direction == TranslateDirection.STRAIGHT)
-//			{
-//				String[] nativeWords = nativeWord.split(";");
-//				int cnt = nativeWords.length;
-//				for(int i=0;i<cnt;i++)
-//				{
-//					isRight = wordEqual(answer,nativeWords[i]);
-//					if(isRight) break;
-//				}
-//			}
-//			else
-//			{
-//				isRight = wordEqual(answer,foreignWord);
-//			}
-			
+			// if (direction == TranslateDirection.STRAIGHT)
+			// {
+			// String[] nativeWords = nativeWord.split(";");
+			// int cnt = nativeWords.length;
+			// for(int i=0;i<cnt;i++)
+			// {
+			// isRight = wordEqual(answer,nativeWords[i]);
+			// if(isRight) break;
+			// }
+			// }
+			// else
+			// {
+			// isRight = wordEqual(answer,foreignWord);
+			// }
+
 			if (isRight) {
 				setLearned(true);
 			}
 			return isRight;
 		}
 	}
-//	private int  maxScore;
-//	private int  incScore;
-//	private int  decScore;
+
+	// private int maxScore;
+	// private int incScore;
+	// private int decScore;
 	/**
 	 * storage of the word
 	 */
 	private Vector<IWord> items;
-	
+
 	/**
 	 * fabric method creating the new Word
 	 */
-	public IWord createWord()
-	{
+	public IWord createWord() {
 		return new WordElem();
 	}
+
 	/**
-	 * fabric method creating the new Word
-	  *   returns instance of created with  foreignWord foreign spelling
-	  *   and nativeWord native spelling
-       */
-	public IWord createWord(String foreignWord,String nativeWord,int idSound)
-	{
+	 * fabric method creating the new Word returns instance of created with
+	 * foreignWord foreign spelling and nativeWord native spelling
+	 */
+	public IWord createWord(String foreignWord, String nativeWord, int idSound) {
 		return new WordElem(foreignWord, nativeWord, idSound);
 	}
-	
-	public void add(String foreignWord, String nativeWord, int idSound)
-	{
+
+	public void add(String foreignWord, String nativeWord, int idSound) {
 		IWord newWord = new WordElem(foreignWord, nativeWord, idSound);
 		items.add(newWord);
 	}
-	
-	public WordVectorContainer()
-	{
-      items = new Vector<IWord>();		
+
+	public WordVectorContainer() {
+		items = new Vector<IWord>();
 	}
-	 public void add(IWord elem)
-	 {
-		items.add(elem); 
-	 }
-	 public IWord get(int i) 
-	 {
-		 return items.get(i);
-     }
-	 public int size()
-	 {
-		 return items.size();
-	 }
-	 public void clear()
-	 {
-		items.clear(); 
-	 }
-	 public void fill(IWordContainer container)
-	 {
-		 int cnt = container.size();
-		 for(int i=0;i<cnt;i++)
-		 {
-			 add(container.get(i));
-		 }	 
-	 }
-	 
-	 public void remove(int index)
-	 {
-		 items.remove(index);
-	 }
+
+	public void add(IWord elem) {
+		items.add(elem);
+	}
+
+	public IWord get(int i) {
+		return items.get(i);
+	}
+
+	public int size() {
+		return items.size();
+	}
+
+	public void clear() {
+		items.clear();
+	}
+
+	public void fill(IWordContainer container) {
+		int cnt = container.size();
+		for (int i = 0; i < cnt; i++) {
+			add(container.get(i));
+		}
+	}
+
+	public void remove(int index) {
+		items.remove(index);
+	}
 
 }
