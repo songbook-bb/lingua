e3cd2b0b978ea1812459e38780f0b679833f76e3
naqn <tomek.nakonieczny@gmail.com>
2014-08-20 12:15:55 +0000

    fix variable naming convention/static
    git-svn-id: https://svn.code.sf.net/p/lingmem/code-0/trunk@39 a1c67283-34fb-429b-9957-cc89a7e5b9eb

--- src/wordtutor/util/keybuttons/KeyboardCtrlPanel.java
+++ src/wordtutor/util/keybuttons/KeyboardCtrlPanel.java
@@ -18,83 +18,83 @@ public class KeyboardCtrlPanel {
   public static KeyboardButtonType langValue;
   Logger logger = Logger.getLogger(KeyboardCtrlPanel.class);
   // HISZPAŃSKIE
-  final String EX = "\u00A1";
-  final String QU = "\u00BF";
-  final String AA = "\u00E1";
-  final String EA = "\u00E9";
-  final String IA = "\u00ED";
-  final String NA = "\u00F1";
-  final String OA = "\u00F3";
-  final String UA = "\u00FA";
-  final String UU = "\u00FC";
+  final static String ex = "\u00A1";
+  final static String qu = "\u00BF";
+  final static String aa = "\u00E1";
+  final static String ea = "\u00E9";
+  final static String ia = "\u00ED";
+  final static String na = "\u00F1";
+  final static String oa = "\u00F3";
+  final static String ua = "\u00FA";
+  final static String uu = "\u00FC";
   // NIEMIECKIE
-  final String SS = "\u00DF";
-  final String OD = "\u00F6";
-  final String UD = "\u00FC";
-  final String AD = "\u00E4";
+  final static String ss = "\u00DF";
+  final static String od = "\u00F6";
+  final static String ud = "\u00FC";
+  final static String ad = "\u00E4";
   // POLSKIE
-  final String AP = "\u0105";
-  final String CP = "\u0107";
-  final String EP = "\u0119";
-  final String LP = " \u0142 ";
-  final String OP = "\u00F3";
-  final String SP = "\u015B";
-  final String ZP = "\u017C";
-  final String XP = "\u017A";
+  final static String ap = "\u0105";
+  final static String cp = "\u0107";
+  final static String ep = "\u0119";
+  final static String lp = " \u0142 ";
+  final static String op = "\u00F3";
+  final static String sp = "\u015B";
+  final static String zp = "\u017C";
+  final static String xp = "\u017A";
   // KASZUBSKIE
-  final String AK = "\u00E3";
-  final String EK = "\u00E9";
-  final String EC = "\u00EB";
-  final String OK = "\u00F2";
-  final String OC = "\u00F4";
-  final String UK = "\u00F9";
+  final static String ak = "\u00E3";
+  final static String ek = "\u00E9";
+  final static String ec = "\u00EB";
+  final static String ok = "\u00F2";
+  final static String oc = "\u00F4";
+  final static String uk = "\u00F9";
   // WŁOSKIE
-  final String AW = "\u00E0";
-  final String AWW = "\u00E1";  
-  final String EW = "\u00E8";
-  final String EV = "\u00E9";
-  final String IW = "\u00EC";
-  final String IWW = "\u00ED";  
-  final String OW = "\u00F2";
-  final String OV = "\u00F3";
-  final String UW = "\u00F9";
-  final String UWW = "\u00FA";  
+  final static String aw = "\u00E0";
+  final static String aww = "\u00E1";  
+  final static String ew = "\u00E8";
+  final static String ev = "\u00E9";
+  final static String iw = "\u00EC";
+  final static String iww = "\u00ED";  
+  final static String ow = "\u00F2";
+  final static String ov = "\u00F3";
+  final static String uw= "\u00F9";
+  final static String uww = "\u00FA";  
   // FRANCUSKIE
-  final String AF = "\u00E0";
-  final String AFF = "\u00E2";
-  final String CF = "\u00E7";
-  final String EF = "\u00E9";
-  final String EFF = "\u00E8";
-  final String EFFF = "\u00EA";
-  final String EFFFF = "\u00EB";  
-  final String IF = "\u00EE";
-  final String IFF = "\u00EF";  
-  final String OF = "\u00F4";
-  final String UF = "\u00FB";
-  final String UFF = "\u00F9";
-  final String UFFF = "\u00FC"; 
-  final String YF = "\u00FF";
-  final String DAF = "\u00E6";
+  final static String af = "\u00E0";
+  final static String aff = "\u00E2";
+  final static String cf = "\u00E7";
+  final static String ef = "\u00E9";
+  final static String eff = "\u00E8";
+  final static String efff = "\u00EA";
+  final static String effff = "\u00EB";  
+  final static String iff = "\u00EE";
+  final static String ifff = "\u00EF";  
+  final static String of = "\u00F4";
+  final static String uf = "\u00FB";
+  final static String uff = "\u00F9";
+  final static String ufff = "\u00FC"; 
+  final static String yf = "\u00FF";
+  final static String daf = "\u00E6";
   // CHORWACKIE
-  final String SHR = "\u0161";
-  final String DHR = "\u0111";    
-  final String CHR = "\u010D";
-  final String CHRR = "\u0107";  
-  final String ZHR = "\u017E";
+  final static String shr = "\u0161";
+  final static String dhr = "\u0111";    
+  final static String chr = "\u010D";
+  final static String chrr = "\u0107";  
+  final static String zhr = "\u017E";
   // LITEWSKIE
-  final String AL = "\u0105";
-  final String CL = "\u010D";    
-  final String EL = "\u0119";
-  final String ELL = "\u0117";  
-  final String IL = "\u012F";
-  final String SL = "\u0161";
-  final String UL = "\u0173";
-  final String ULL = "\u016B";
-  final String ZL = "\u017E";
+  final static String al = "\u0105";
+  final static String cl = "\u010D";    
+  final static String el = "\u0119";
+  final static String ell = "\u0117";  
+  final static String il = "\u012F";
+  final static String sl = "\u0161";
+  final static String ul = "\u0173";
+  final static String ull = "\u016B";
+  final static String zl = "\u017E";
   // NORWESKIE 
-  final String NAE = "\u00E6";
-  final String NO = "\u00F8";    
-  final String NNA = "\u00E5";
+  final static String nae = "\u00E6";
+  final static String no = "\u00F8";    
+  final static String nna = "\u00E5";
   // NEW KEYBOARD KEYS DEFINITION HERE
   private final JPanel pLetters;
 
@@ -121,88 +121,88 @@ public class KeyboardCtrlPanel {
 
     if (langEnum == KeyboardButtonType.SPANISH) {
       // HISZPAŃSKIE
-      addButtonToPanel(QU, pLetters);
-      addButtonToPanel(EX, pLetters);
-      addButtonToPanel(AA, pLetters);
-      addButtonToPanel(EA, pLetters);
-      addButtonToPanel(IA, pLetters);
-      addButtonToPanel(NA, pLetters);
-      addButtonToPanel(OA, pLetters);
-      addButtonToPanel(UA, pLetters);
-      addButtonToPanel(UU, pLetters);
+      addButtonToPanel(qu, pLetters);
+      addButtonToPanel(ex, pLetters);
+      addButtonToPanel(aa, pLetters);
+      addButtonToPanel(ea, pLetters);
+      addButtonToPanel(ia, pLetters);
+      addButtonToPanel(na, pLetters);
+      addButtonToPanel(oa, pLetters);
+      addButtonToPanel(ua, pLetters);
+      addButtonToPanel(uu, pLetters);
     } else if (langEnum == KeyboardButtonType.GERMAN) {
       // NIEMIECKIE
-      addButtonToPanel(SS, pLetters);
-      addButtonToPanel(OD, pLetters);
-      addButtonToPanel(UD, pLetters);
-      addButtonToPanel(AD, pLetters);
+      addButtonToPanel(ss, pLetters);
+      addButtonToPanel(od, pLetters);
+      addButtonToPanel(ud, pLetters);
+      addButtonToPanel(ad, pLetters);
     } else if (langEnum == KeyboardButtonType.POLISH) {
       // POLSKIE
-      addButtonToPanel(AP, pLetters);
-      addButtonToPanel(CP, pLetters);
-      addButtonToPanel(EP, pLetters);
-      addButtonToPanel(LP, pLetters);
-      addButtonToPanel(OP, pLetters);
-      addButtonToPanel(SP, pLetters);
-      addButtonToPanel(ZP, pLetters);
-      addButtonToPanel(XP, pLetters);
+      addButtonToPanel(ap, pLetters);
+      addButtonToPanel(cp, pLetters);
+      addButtonToPanel(ep, pLetters);
+      addButtonToPanel(lp, pLetters);
+      addButtonToPanel(op, pLetters);
+      addButtonToPanel(sp, pLetters);
+      addButtonToPanel(zp, pLetters);
+      addButtonToPanel(xp, pLetters);
     } else if (langEnum == KeyboardButtonType.KASHUBIAN) {
       // KASZUBSKIE
-      addButtonToPanel(AK, pLetters);
-      addButtonToPanel(EK, pLetters);
-      addButtonToPanel(EC, pLetters);
-      addButtonToPanel(OK, pLetters);
-      addButtonToPanel(OC, pLetters);
-      addButtonToPanel(UK, pLetters);
+      addButtonToPanel(ak, pLetters);
+      addButtonToPanel(ek, pLetters);
+      addButtonToPanel(ec, pLetters);
+      addButtonToPanel(ok, pLetters);
+      addButtonToPanel(oc, pLetters);
+      addButtonToPanel(uk, pLetters);
     } else if (langEnum == KeyboardButtonType.ITALIAN) {
       // WŁOSKIE
-      addButtonToPanel(AW, pLetters);
-      addButtonToPanel(AWW, pLetters);      
-      addButtonToPanel(EW, pLetters);
-      addButtonToPanel(EV, pLetters);
-      addButtonToPanel(IW, pLetters);
-      addButtonToPanel(IWW, pLetters);      
-      addButtonToPanel(OW, pLetters);
-      addButtonToPanel(OV, pLetters);
-      addButtonToPanel(UW, pLetters);
-      addButtonToPanel(UWW, pLetters);      
+      addButtonToPanel(aw, pLetters);
+      addButtonToPanel(aww, pLetters);      
+      addButtonToPanel(ew, pLetters);
+      addButtonToPanel(ev, pLetters);
+      addButtonToPanel(iw, pLetters);
+      addButtonToPanel(iww, pLetters);      
+      addButtonToPanel(ow, pLetters);
+      addButtonToPanel(ov, pLetters);
+      addButtonToPanel(uw, pLetters);
+      addButtonToPanel(uww, pLetters);      
     } else if (langEnum == KeyboardButtonType.FRENCH) {
       // FRANCUSKIE
-      addButtonToPanel(AF, pLetters);
-      addButtonToPanel(AFF, pLetters);
-      addButtonToPanel(CF, pLetters);
-      addButtonToPanel(EF, pLetters);
-      addButtonToPanel(EFF, pLetters);
-      addButtonToPanel(EFFF, pLetters);
-      addButtonToPanel(EFFFF, pLetters);
-      addButtonToPanel(IF, pLetters);
-      addButtonToPanel(IFF, pLetters);
-      addButtonToPanel(OF, pLetters);
-      addButtonToPanel(UF, pLetters);
-      addButtonToPanel(UFF, pLetters);
-      addButtonToPanel(UFFF, pLetters);
-      addButtonToPanel(YF, pLetters);
-      addButtonToPanel(DAF, pLetters);
+      addButtonToPanel(af, pLetters);
+      addButtonToPanel(aff, pLetters);
+      addButtonToPanel(cf, pLetters);
+      addButtonToPanel(ef, pLetters);
+      addButtonToPanel(eff, pLetters);
+      addButtonToPanel(efff, pLetters);
+      addButtonToPanel(effff, pLetters);
+      addButtonToPanel(iff, pLetters);
+      addButtonToPanel(ifff, pLetters);
+      addButtonToPanel(of, pLetters);
+      addButtonToPanel(uf, pLetters);
+      addButtonToPanel(uff, pLetters);
+      addButtonToPanel(ufff, pLetters);
+      addButtonToPanel(yf, pLetters);
+      addButtonToPanel(daf, pLetters);
     } else if (langEnum == KeyboardButtonType.CROATIAN) {
-        addButtonToPanel(SHR, pLetters);
-        addButtonToPanel(DHR, pLetters);
-        addButtonToPanel(CHR, pLetters);
-        addButtonToPanel(CHRR, pLetters);
-        addButtonToPanel(ZHR, pLetters);        
+        addButtonToPanel(shr, pLetters);
+        addButtonToPanel(dhr, pLetters);
+        addButtonToPanel(chr, pLetters);
+        addButtonToPanel(chrr, pLetters);
+        addButtonToPanel(zhr, pLetters);        
     } else if (langEnum == KeyboardButtonType.LITHUANIAN) {
-    		addButtonToPanel(AL, pLetters);
-        addButtonToPanel(CL, pLetters);
-        addButtonToPanel(EL, pLetters);
-        addButtonToPanel(ELL, pLetters);
-        addButtonToPanel(IL, pLetters);        
-        addButtonToPanel(SL, pLetters);
-        addButtonToPanel(UL, pLetters);
-        addButtonToPanel(ULL, pLetters);
-        addButtonToPanel(ZL, pLetters);        
+    		addButtonToPanel(al, pLetters);
+        addButtonToPanel(cl, pLetters);
+        addButtonToPanel(el, pLetters);
+        addButtonToPanel(ell, pLetters);
+        addButtonToPanel(il, pLetters);        
+        addButtonToPanel(sl, pLetters);
+        addButtonToPanel(ul, pLetters);
+        addButtonToPanel(ull, pLetters);
+        addButtonToPanel(zl, pLetters);        
     } else if (langEnum == KeyboardButtonType.NORWEGIAN) {
-  		addButtonToPanel(NAE, pLetters);
-      addButtonToPanel(NO, pLetters);
-      addButtonToPanel(NNA, pLetters);    	
+  		addButtonToPanel(nae, pLetters);
+      addButtonToPanel(no, pLetters);
+      addButtonToPanel(nna, pLetters);    	
     }
     // ADD NEW KEYBOARD PANEL
   }
@@ -210,9 +210,9 @@ public class KeyboardCtrlPanel {
   private void addButtonToPanel(String buttonName, JPanel panel) {
     JButton button = new JButton(buttonName);
     button.setFocusable(false);
-    if (buttonName == QU) {
+    if (buttonName == qu) {
       button.setToolTipText(Util.getLocalizedString("LESSON.BUTTON.QU.MARK"));
-    } else if (buttonName == EX) {
+    } else if (buttonName == ex) {
       button.setToolTipText(Util.getLocalizedString("LESSON.BUTTON.EX.MARK"));
     }
     panel.add(button);
